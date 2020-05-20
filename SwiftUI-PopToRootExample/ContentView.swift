cef5cd186254ac1ec7682d5b6df9ca1cdcfb74b6
Chuck Hartman <Chuck@ForeTheGreen.com>
2020-05-20 12:49:37 -0700

    Initial Commit

---
+++ SwiftUI-PopToRootExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-PopToRootExample
+//
+//  Created by Chuck Hartman on 5/20/20.
+//  Copyright © 2020 ForeTheGreen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
